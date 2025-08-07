1617964280e33740f3172b7e4d1ce576bd5f8a60
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-07 20:55:19 +0300

    add model layer

---
+++ src/main/java/com/Sprints/BankManagementSystem/Model/Customer.java
@@ -0,0 +1,84 @@
+package com.Sprints.BankManagementSystem.Model;
+
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long customer_id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true)
+    private String phone;
+    private String address;
+
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL)
+    private List<BankAccount>accounts;
+
+    public Customer() {
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAccounts(List<BankAccount> accounts) {
+        this.accounts = accounts;
+    }
+    public void setCustomer_id(Long customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getCustomer_id() {
+        return customer_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+}
---
+++ src/main/java/com/Sprints/BankManagementSystem/Model/Transaction.java
@@ -0,0 +1,77 @@
+package com.Sprints.BankManagementSystem.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long transaction_id;
+    private String type;     //deposit, withdrawal,transfer
+    private Double amount;
+    private LocalDateTime timestamp;
+
+
+    @ManyToOne
+    @JoinColumn(name = "sender_account_id")
+    private BankAccount sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_account_id")
+    private BankAccount receiver;
+
+    public Transaction() {
+    }
+
+
+    public void setTransaction_id(Long transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setSender(BankAccount sender) {
+        this.sender = sender;
+    }
+
+    public void setReceiver(BankAccount receiver) {
+        this.receiver = receiver;
+    }
+
+    public Long getTransaction_id() {
+        return transaction_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public BankAccount getSender() {
+        return sender;
+    }
+
+    public BankAccount getReceiver() {
+        return receiver;
+    }
+}
