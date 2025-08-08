a005589df2eba3b188b284f8ed6c8d435d4b8a5a
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-08 20:36:41 +0300

    Update CustomerService.java

--- src/main/java/com/Sprints/BankManagementSystem/Service/Customer/CustomerService.java
+++ src/main/java/com/Sprints/BankManagementSystem/Service/Customer/CustomerService.java
@@ -1,4 +1,66 @@
 package com.Sprints.BankManagementSystem.Service.Customer;
 
-public class CustomerService {
+import com.Sprints.BankManagementSystem.DTO.CustomerDto;
+import com.Sprints.BankManagementSystem.Mapper.CustomerMapper;
+import com.Sprints.BankManagementSystem.Model.Customer;
+import com.Sprints.BankManagementSystem.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerService implements ICustomerService{
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository, CustomerMapper customerMapper) {
+        this.customerRepository = customerRepository;
+        this.customerMapper = customerMapper;
+    }
+
+    @Override
+    public CustomerDto createCustomer(CustomerDto customerDto) {
+        Customer customer = customerMapper.toEntity(customerDto);
+        customer = customerRepository.save(customer);
+        return customerMapper.toDTO(customer);
+    }
+
+    @Override
+    public CustomerDto getCustomerById(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+        return customerMapper.toDTO(customer);
+    }
+
+    @Override
+    public List<CustomerDto> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream()
+                .map(customerMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDto updateCustomer(Long id, CustomerDto customerDto) {
+        Customer existingCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+
+        existingCustomer.setName(customerDto.getName());
+        existingCustomer.setEmail(customerDto.getEmail());
+        existingCustomer.setPhone(customerDto.getPhone());
+
+        Customer updated = customerRepository.save(existingCustomer);
+        return customerMapper.toDTO(updated);
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        if (!customerRepository.existsById(id)) {
+            throw new RuntimeException("Customer not found with ID: " + id);
+        }
+        customerRepository.deleteById(id);
+    }
 }
