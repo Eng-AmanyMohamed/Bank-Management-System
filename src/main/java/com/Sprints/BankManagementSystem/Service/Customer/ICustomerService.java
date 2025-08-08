e9454e2e6998281213f44dce45cebc979c9cbbe0
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-08 20:35:19 +0300

    Update ICustomerService.java

--- src/main/java/com/Sprints/BankManagementSystem/Service/Customer/ICustomerService.java
+++ src/main/java/com/Sprints/BankManagementSystem/Service/Customer/ICustomerService.java
@@ -1,4 +1,14 @@
 package com.Sprints.BankManagementSystem.Service.Customer;
 
+import com.Sprints.BankManagementSystem.DTO.CustomerDto;
+
+import java.util.List;
+
 public interface ICustomerService {
+
+    CustomerDto createCustomer(CustomerDto customerDto);
+    CustomerDto getCustomerById(Long id);
+    List<CustomerDto> getAllCustomers();
+    CustomerDto updateCustomer(Long id, CustomerDto customerDto);
+    void deleteCustomer(Long id);
 }
