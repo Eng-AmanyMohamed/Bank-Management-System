df70d120599c8b0a5adc5cda9f2920a955bc5236
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-08-09 15:52:09 +0300

    Update TransactionDto.java

--- src/main/java/com/Sprints/BankManagementSystem/DTO/TransactionDto.java
+++ src/main/java/com/Sprints/BankManagementSystem/DTO/TransactionDto.java
@@ -9,18 +9,21 @@ public class TransactionDto {
     private LocalDateTime timestamp;
     private Long senderAccountId;
     private Long receiverAccountId;
-
+    private Long customer_id;
     public TransactionDto() {
     }
 
-    public TransactionDto(Long transaction_id, String type, Double amount, LocalDateTime timestamp, Long senderAccountId, Long receiverAccountId) {
+    public TransactionDto(Long transaction_id, String type, Double amount, LocalDateTime timestamp, Long senderAccountId, Long receiverAccountId,Long customer_id) {
         this.transaction_id = transaction_id;
         this.type = type;
         this.amount = amount;
         this.timestamp = timestamp;
         this.senderAccountId = senderAccountId;
         this.receiverAccountId = receiverAccountId;
+        this.customer_id = customer_id;
     }
+
+    public void setCustomer_id(Long customer_id) {this.customer_id = customer_id;}
 
     public void setSenderAccountId(Long senderAccountId) {
         this.senderAccountId = senderAccountId;
@@ -69,4 +72,6 @@ public class TransactionDto {
     public Long getReceiverAccountId() {
         return receiverAccountId;
     }
+
+    public Long getCustomer_id() {return customer_id;}
 }
